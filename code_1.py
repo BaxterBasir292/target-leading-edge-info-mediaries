09ae54d632d656f42996fadb20ce66beddf8a98f
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-09-28 08:00:55 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,46 +1,30 @@
-    for (let i = 0; i < 10; i++) {
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-function generateRandomData() {
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
 
-}
-    data.forEach(item => {
-    return data;
-    }
-        console.log("Random Number: " + item);
-main();
-
-}
-    const data = [];
-
-    const data = generateRandomData();if __name__ == "__main__":
 def main():
-
     main()
 
     return data
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    data = generate_random_data()
     for item in data:
         print(f"Random Number: {item}")
-import random
+if __name__ == "__main__":
 
-    data.forEach(item => {
-function generateRandomData() {
+
+def generate_random_data():
+import random    const data = generateRandomData();
     });
-function main() {
-    }
-    const data = generateRandomData();
+function generateRandomData() {
 
-    return data;
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-}
         console.log("Random Number: " + item);
-main();
-        data.push(Math.floor(Math.random() * 100) + 1);
 
+function main() {
 }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    }
+}
+    return data;
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+
+main();
