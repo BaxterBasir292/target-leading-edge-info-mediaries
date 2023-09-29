70e2ff76a948f674eca4ab073f707a81f90077ca
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-09-29 02:02:19 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,53 +1,38 @@
-
-
-    data = generate_random_data()
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-    for item in data:
-def generate_random_data():
-import random
-    return data
-
-
         print(f"Random Number: {item}")
-if __name__ == "__main__":    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-import random
-        print(f"Random Number: {item}")
-
-
 def main():
-
-def generate_random_data():
     main()
+import random
 
-    data = generate_random_data()
-    return data        print(f"Random Number: {item}")
-
-def main():
-    data = generate_random_data()
-
+    for item in data:
     return data
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def generate_random_data():
-
-    for item in data:
-import random
-
-if __name__ == "__main__":def generate_random_data():
-    main()
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
 
 
-
-    return data
-import random
-
-        print(f"Random Number: {item}")
-def main():
     data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():        int[] data = new int[10];
+
+        for (int item : data) {
+public class RandomDataGenerator {
+        }
+        for (int i = 0; i < 10; i++) {
+    }
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        }
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+
+    }
+        int[] data = new int[10];
+    public static void main(String[] args) {
+        for (int item : data) {
+        }
+}
+        for (int i = 0; i < 10; i++) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
