2efe6c06ccd273216adf078be960b7474b0080b4
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-09-29 11:03:00 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,30 +1,14 @@
-    data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
 
 def main():
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+def generate_random_data():
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
     main()
 
     return data
-    for item in data:
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-
-
-def generate_random_data():
-import random    const data = generateRandomData();
-    });
-function generateRandomData() {
-
-        console.log("Random Number: " + item);
-
-function main() {
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = [];
-    }
-}
-    return data;
-    data.forEach(item => {
-    for (let i = 0; i < 10; i++) {
-
-main();
