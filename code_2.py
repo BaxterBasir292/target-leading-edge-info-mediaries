4f8ced39b1765e39f94304f626776db9144a09b1
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-10-01 08:06:27 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,52 +1,14 @@
-        print(f"Random Number: {item}")
-def main():
-
+    data = [random.randint(1, 100) for _ in range(10)]
     main()
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+import random
+
     return data
 def generate_random_data():
     data = generate_random_data()
-
-import random
-    for item in data:
-if __name__ == "__main__":
-
-
-    data = [random.randint(1, 100) for _ in range(10)]import random
-
-    data = generate_random_data()
-if __name__ == "__main__":
-    main()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-        print(f"Random Number: {item}")
 def main():
-    for item in data:
-
-def generate_random_data():
-if __name__ == "__main__":
 
     for item in data:
-import random
-
-
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def generate_random_data():
-
-        print(f"Random Number: {item}")
-def main():
-    return data        int[] data = new int[10];
-public class RandomDataGenerator {
-
-        }
-        for (int item : data) {
-}
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
-    }
