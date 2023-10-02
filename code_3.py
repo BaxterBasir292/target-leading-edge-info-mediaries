b2a9ba37bace529e06e2df4de857de9699dcb2af
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-10-02 02:07:49 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,26 +1,63 @@
+        }
+
+
+        for (int item : data) {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+    }
+        int[] data = new int[10];
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+}        }
+}
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+
+public class RandomDataGenerator {
+    }
+
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;    main()
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    data = generate_random_data()
 import random
 
+
 def generate_random_data():
-
-    for item in data:
-    return data
-
-def main():
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
         print(f"Random Number: {item}")
-    data = generate_random_data()
-import random
-def main():
-
-
     return data
-
-    main()
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
     for item in data:
+def main():        }
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+public class RandomDataGenerator {
+        for (int item : data) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
+
+    }
+}def main():
+    main()
 def generate_random_data():
+
+    for item in data:
+    return data
+if __name__ == "__main__":
+import random
+
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
         print(f"Random Number: {item}")
